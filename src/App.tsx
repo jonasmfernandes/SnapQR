7b21ee3772553564e87b5809a86b46f558658110
Jonas Monteiro <mfernandes.jonas@gmail.com>
2025-02-03 20:08:12 -0300

    feat: started project

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-[#f8f8f8] h-screen flex justify-center items-center">
+        <div className="bg-[#1f1f1f] inline-flex items-center flex-col align-center p-4 gap-3 rounded-md">
+          <input type="text" className="rounded-md bg-white" />
+          <button className="flex items-center gap-1 py-2 px-5 rounded-md mt-2 bg-[#404040] **:duration-200 cursor-pointer">
+            Generate
+          </button>
+            
+    
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App

b7e3e4332f5cf7d91633dd5cf4cea3d944c05b1a
Jonas Monteiro <mfernandes.jonas@gmail.com>
2025-02-24 20:32:07 -0300

    feat: started ui register page

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,9 +1,43 @@
-import './App.css'
+import "./App.css";
+import { QrCode } from "lucide-react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <div className=" bg-white flex">
+        <section className="h-screen w-[50%] flex justify-center items-center">
+          <main className="flex flex-col gap-2">
+            <QrCode size={64} />
+            <h1 className="font-bold text-3xl pt-4">Get Started</h1>
+            <h4 className="text-black/50">
+              Welcome to SnapQR - Let's create your account
+            </h4>
+            <div className="bg-[#68455775] h-0.5 rounded-md w-96 my-5"></div>
 
-export default Login
+            <h2>Email</h2>
+            <input
+              type="email"
+              className="rounded-md bg-transparent p-2 border border-[#d6d2b5] focus:outline-none focus:border-[#684557] hover:border-[#684557] shadow-sm focus:shadow transition duration-100"
+              placeholder="Digite aqui seu e-mail"
+            />
+
+            <h2>Senha</h2>
+            <input
+              type="password"
+              className="rounded-md bg-transparent p-2 border mb-4 border-[#d6d2b5] focus:outline-none focus:border-[#684557] hover:border-[#684557] shadow-sm focus:shadow transition duration-100"
+            />
+
+            <button 
+            className="flex items-center justify-center gap-1 py-2 px-5 rounded-md mt-2 duration-200 text-sm bg-gradient-to-r from-[#2e1b25] to-[#684557]  text-white cursor-pointer hover:"
+            >Sign Up</button>
+            <p className="text-black/50 text-center">Já tem uma conta? <a href="#" className="text-[#181818] transition duration-100 hover:text-black">Entre aqui</a></p>
+          </main>
+
+        </section>
+        <section className="bg-[#684557] h-screen w-[50%] rounded-l-4xl"></section>
+      </div>
+    </>
+  );
+};
+
+export default Login;

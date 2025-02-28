1045553bce900e05cbf10760d57a5f5057a0da58
Jonas Monteiro <mfernandes.jonas@gmail.com>
2025-02-28 20:11:45 -0300

    feat: created /register endpoint

--- src/back-end/index.cjs
+++ src/back-end/index.cjs
@@ -2,7 +2,8 @@ require("dotenv").config()
 
 const express = require('express')
 const cors = require('cors')
-
+const bcrypt = require('bcryptjs')
+const { MongoClient } = require('mongodb')
 const app = express()
 
 app.use(express.json())
@@ -14,3 +15,37 @@ app.get("/", (req,res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`server is working on console.log. port:${PORT}`))
+
+app.post('/register', async(req, res) => {
+    const { email, password, confirmPassword } = req.body;
+
+    if (!email || !password || !confirmPassword){
+        return res.status(400).json({error: "Todos os campos são obrigatórios."})
+    }
+
+    if ( password !== confirmPassword){
+        return res.status(400).json({error: "As senhas não coincidem."})
+    }
+
+    try {
+        const client = await MongoClient.connect("mongodb://localhost:27017")
+        const db = client.db('snapqr')
+        const users = db.collection('users')
+
+        const userExists = await users.findOne({ email });
+        if(userExists){
+            return res.status(400).json({error: "E-mail já registrado"})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const newUser = { email, password:hashedPassword }
+        await users.insertOne(newUser)
+
+        res.status(201).json({message: "Usuário registrado com sucesso."})
+
+        client.close()
+    } catch(error) {
+        res.status(500).json({ error: "Erro ao registrar usuário."})
+    }
+})

ef94f3e4a91d0593acda12142be002aa68b03932
Jonas Monteiro <mfernandes.jonas@gmail.com>
2025-03-03 11:26:55 -0300

    feat: added /login endpoint

--- src/back-end/index.cjs
+++ src/back-end/index.cjs
@@ -1,51 +1,81 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const express = require('express')
-const cors = require('cors')
-const bcrypt = require('bcryptjs')
-const { MongoClient } = require('mongodb')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const { MongoClient } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get("/", (req,res) => {
-    res.send("server is working.")
-})
+app.get("/", (req, res) => {
+  res.send("server is working.");
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> console.log(`server is working on console.log. port:${PORT}`))
+app.listen(PORT, () =>
+  console.log(`server is working on console.log. port:${PORT}`)
+);
 
-app.post('/register', async(req, res) => {
-    const { email, password, confirmPassword } = req.body;
+app.post("/register", async (req, res) => {
+  const { email, password, confirmPassword } = req.body;
 
-    if (!email || !password || !confirmPassword){
-        return res.status(400).json({error: "Todos os campos são obrigatórios."})
+  if (!email || !password || !confirmPassword) {
+    return res.status(400).json({ error: "Todos os campos são obrigatórios." });
+  }
+
+  if (password !== confirmPassword) {
+    return res.status(400).json({ error: "As senhas não coincidem." });
+  }
+
+  try {
+    const client = await MongoClient.connect("mongodb://localhost:27017");
+    const db = client.db("snapqr");
+    const users = db.collection("users");
+
+    const userExists = await users.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ error: "E-mail já registrado" });
     }
 
-    if ( password !== confirmPassword){
-        return res.status(400).json({error: "As senhas não coincidem."})
-    }
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    try {
-        const client = await MongoClient.connect("mongodb://localhost:27017")
-        const db = client.db('snapqr')
-        const users = db.collection('users')
+    const newUser = { email, password: hashedPassword };
+    await users.insertOne(newUser);
 
-        const userExists = await users.findOne({ email });
-        if(userExists){
-            return res.status(400).json({error: "E-mail já registrado"})
-        }
+    res.status(201).json({ message: "Usuário registrado com sucesso." });
 
-        const hashedPassword = await bcrypt.hash(password, 10)
+    client.close();
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao registrar usuário." });
+  }
+});
 
-        const newUser = { email, password:hashedPassword }
-        await users.insertOne(newUser)
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const client = await MongoClient.connect("mongodb://localhost:27017");
+  const db = client.db("snapqr");
+  const users = db.collection("users");
+  if (!email || !password) {
+    return res.status(400).json({ error: "Todos os campos são obrigatórios" });
+  }
 
-        res.status(201).json({message: "Usuário registrado com sucesso."})
+  const user = await users.findOne({ email });
 
-        client.close()
-    } catch(error) {
-        res.status(500).json({ error: "Erro ao registrar usuário."})
-    }
-})
+  if (!user) {
+    return res.status(404).json({ error: "Usuário não encontrado" });
+  }
+
+  const correctPassword = await bcrypt.compare(password, user.password);
+  if (!correctPassword) {
+    return res.status(401).json({ error: "Senha incorreta." });
+  }
+
+  const token = jwt.sign({ id: user._id, email: user.email }, "secreto", {
+    expiresIn: "1h",
+  });
+
+  res.json({ message: "Login bem-sucedido!", token });
+});

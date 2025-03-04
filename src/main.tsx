8ce7e9d313bb556d8b163dff5c73263a9f08125a
Jonas Monteiro <mfernandes.jonas@gmail.com>
2025-03-04 20:08:28 -0300

    feat: added /register init on localhost

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 import Register from './Register.tsx'
@@ -9,6 +9,7 @@ createRoot(document.getElementById('root')!).render(
 
   <BrowserRouter>
   <Routes>
+    <Route path="/" element={<Navigate to="/register" replace />}/>
     <Route path="/dashboard" element={<App />}/>
     <Route path="/register" element={<Register />}/>
     <Route path="/login" element={<Login />} />

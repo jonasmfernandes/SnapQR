5f9a81cf745af830208af04aba228539be70c125
Jonas Monteiro <mfernandes.jonas@gmail.com>
2025-03-17 16:10:26 -0300

    fix: changed url fetch

--- src/Register.tsx
+++ src/Register.tsx
@@ -14,7 +14,7 @@ const Register = () => {
 
   const handleRegister = async () => {
     try {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}register`, {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/register`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
